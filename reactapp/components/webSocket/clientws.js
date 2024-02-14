9c59783ef9db859f44051c13accd2f1bdb7358e7
romer8 <gio.busrom@gmail.com>
2024-02-14 09:50:22 -0700

    RefactoringCode: WebSocket attempt, not functional

---
+++ reactapp/components/webSocket/clientws.js
@@ -0,0 +1,73 @@
+
+
+const reconnectingSocket = (url) => {
+    let client;
+    let isConnected = false;
+    let reconnectOnClose = true;
+    let messageListeners = [];
+    let stateChangeListeners = [];
+  
+    const on = (fn) => {
+      messageListeners.push(fn);
+    };
+  
+    const off = (fn) => {
+      messageListeners = messageListeners.filter((l) => l !== fn);
+    };
+  
+    const onStateChange = (fn) => {
+      stateChangeListeners.push(fn);
+      return () => {
+        stateChangeListeners = stateChangeListeners.filter((l) => l !== fn);
+      };
+    };
+  
+    const start = () => {
+      client = new WebSocket(url);
+  
+      client.onopen = () => {
+        isConnected = true;
+        stateChangeListeners.forEach((fn) => fn(true));
+      };
+  
+      const close = client.close;
+  
+      // Close without reconnecting;
+      client.close = () => {
+        reconnectOnClose = false;
+        close.call(client);
+      };
+  
+      client.onmessage = (event) => {
+        messageListeners.forEach((fn) => fn(event.data));
+      };
+  
+      client.onerror = (e) => console.error(e);
+  
+      client.onclose = () => {
+        isConnected = false;
+        stateChangeListeners.forEach((fn) => fn(false));
+  
+        if (!reconnectOnClose) {
+          console.log('ws closed by app');
+          return;
+        }
+  
+        console.log('ws closed by server');
+  
+        setTimeout(start, 3000);
+      };
+    };
+  
+    start();
+  
+    return {
+      on,
+      off,
+      onStateChange,
+      close: () => client.close(),
+      getClient: () => client,
+      isConnected: () => isConnected,
+    };
+  };
+  
