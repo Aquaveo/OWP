7b04c15ce5cdd40a58ad0c458c3c68aab630909d
romer8 <gio.busrom@gmail.com>
2024-02-26 16:30:53 -0700

    RefactoCode: adding loading to the previewFile

--- reactapp/features/RegionsForms/components/submenus/GeometryRegion/GeometryFileInput.js
+++ reactapp/features/RegionsForms/components/submenus/GeometryRegion/GeometryFileInput.js
@@ -1,60 +1,53 @@
-import React, { useState, Fragment,useEffect  } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import { FormGroup, Label } from "components/UI/StyleComponents/Form.styled";
-
 import { Controller } from 'react-hook-form';
 import { Input } from "components/UI/StyleComponents/Input.styled";
-import {previewGeometryFileData} from 'features/RegionsForms/lib/fileUtils'; 
+import { previewGeometryFileData } from 'features/RegionsForms/lib/fileUtils'; 
 import { useMapContext } from "features/Map/hooks/useMapContext";
+import { LoadingText } from "components/UI/StyleComponents/Loader.styled";
 
-const GeometryFileInput = (
-    { 
-        isVisible,
-        control
-    }) => {
+const GeometryFileInput = ({ isVisible, control }) => {
     const [geometryLayerRegion, setGeometryLayerRegion] = useState(null);
-    const {actions} = useMapContext();
+    const [isLoadingFilePreview, setIsLoadingFilePreview] = useState(false);
+    const { actions } = useMapContext();
+
     useEffect(() => {
-        console.log(geometryLayerRegion);
         if (!geometryLayerRegion) return;
         actions.addLayer(geometryLayerRegion);
-    }
-    , [geometryLayerRegion]);
+        setIsLoadingFilePreview(false);
+    }, [geometryLayerRegion]);
 
-    return(
-        isVisible 
-            ? 
-                <Fragment>
-                    <FormGroup>
-                        <Label>Upload File (*.shp, *.json, geopackage)</Label>
-                        <Controller
-                            name="files"
-                            control={control}
-                            defaultValue={[]}
-                            render={({ field: { onChange, value, ref } }) => (
-                                <Input
-                                    size="sm"
-                                    type="file"
-                                    multiple
-                                    onChange={async (e) => {
-                                        if (geometryLayerRegion){
-                                            actions.removeLayer(geometryLayerRegion);
-                                        };
-                                        let geometry = await previewGeometryFileData(e);
-                                        setGeometryLayerRegion(geometry);
-                                        // IMPORTANT: Update the form state by calling onChange provided by React Hook Form
-                                        onChange([...e.target.files]); // Pass the files array to update the form state
-                                    }}
-                                    ref={ref}
-                                />
-                                )}
-                            rules={{ required: 'Files are required' }}
+    return isVisible ? (
+        <Fragment>
+            <FormGroup>
+                <Label>Upload File (*.shp, *.json, geopackage)</Label>
+                <Controller
+                    name="files"
+                    control={control}
+                    defaultValue={[]}
+                    render={({ field: { onChange, value, ref } }) => (
+                        <Input
+                            size="sm"
+                            type="file"
+                            multiple
+                            onChange={async (e) => {
+                                setIsLoadingFilePreview(true);
+                                if (geometryLayerRegion) {
+                                    actions.removeLayer(geometryLayerRegion);
+                                }
+                                let geometry = await previewGeometryFileData(e);
+                                setGeometryLayerRegion(geometry);
+                                onChange([...e.target.files]);
+                            }}
+                            ref={ref}
                         />
-                    </FormGroup>
-                
-                </Fragment>
-            : 
-                <></>
-    );
-  };
-  
+                    )}
+                    rules={{ required: 'Files are required' }}
+                />
+            </FormGroup>
+            {isLoadingFilePreview ? <LoadingText>Loading Region Preview ...</LoadingText> : null}
+        </Fragment>
+    ) : null;
+};
+
 export { GeometryFileInput };
