39da89150a3b635065a34f6d9b21f0f2800fcb7d
romer8 <gio.busrom@gmail.com>
2024-04-11 09:07:15 -0600

    Deleting more files that are not being used

--- reactapp/App.js
+++ reactapp/App.js
@@ -8,14 +8,12 @@ import { WebSocketProvider } from 'features/WebSocket/providers/WebSocketProvide
 import NwpProductsProvider from 'features/NwpProducts/providers/NwpProductsProvider';
 import Map from 'features/Map/components/Map';
 import { getWsURL } from 'lib/utils';
-import LearnReact from 'views/learn/LearnReact';
 import 'App.scss';
 
 
 const TETHYS_PREFIX_URL = process.env.TETHYS_PREFIX_URL.replace(/^\/|\/$/g, '');
-const redirect_hydroshare = TETHYS_PREFIX_URL ? `/${TETHYS_PREFIX_URL}/oauth2/login/hydroshare/` : "/oauth2/login/hydroshare/"
 
-
+// const redirect_hydroshare = TETHYS_PREFIX_URL ? `/${TETHYS_PREFIX_URL}/oauth2/login/hydroshare/` : "/oauth2/login/hydroshare/"
 // const webSocketHost = process.env.TETHYS_WEB_SOCKET_HOST
 // const prefix_url = process.env.TETHYS_PREFIX_URL ? `/${process.env.TETHYS_PREFIX_URL.replace(/^\/|\/$/g, '')}/` : '';
 // const app_root_relative_path = process.env.TETHYS_APP_ROOT_URL_RELATIVE ? `${process.env.TETHYS_APP_ROOT_URL_RELATIVE.replace(/^\/|\/$/g, '')}` : '';
@@ -26,7 +24,6 @@ const ws = getWsURL();
 function App() {
   const PATH_HOME = '/',
         PATH_INFO = '/Information/';
-  // const layers = [];
   return (
     <>
       <ErrorBoundary>
@@ -51,7 +48,7 @@ function App() {
                       </Fragment>
                     } 
                       key='route-home' />,
-                  <Route path={PATH_INFO} element={<LearnReact />} key='route-learn' />
+                  
                 ]}
               />
           </Loader>
