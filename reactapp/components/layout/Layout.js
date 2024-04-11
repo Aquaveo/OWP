c1d0ccfb6d97d69a8142a60dede5ee554e032bd8
romer8 <gio.busrom@gmail.com>
2024-04-11 15:58:59 -0600

    new logo and documentation

--- reactapp/components/layout/Layout.js
+++ reactapp/components/layout/Layout.js
@@ -18,7 +18,40 @@ function Layout({navLinks, routes, children}) {
     <div className="h-100">
         <Header onNavChange={setNavVisible} />
         <NavMenu navTitle="About"  navVisible={navVisible} onNavChange={setNavVisible}>
-          <p></p>
+          <h5>Layers</h5>
+          <p>Layers used for this application:
+            <ul>
+              <li><a href="https://server.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer"> Light Gray World BaseMap</a></li>
+              <li><a href="https://mapservices.weather.noaa.gov/vector/rest/services/obs/NWM_Stream_Analysis/MapServer">National Stream Analysis Anomaly</a></li>
+            </ul>
+          </p>
+          <h5>National Water Prediction Service (NWPS) API Data Services</h5>
+          <p>When Clicking on a reach, a modal will pop up with the following time series:</p>
+          <p>
+            <ul>
+              <li>Analysis Assimilation</li>
+              <li>Short Range Forecast</li>
+              <li>Medium Range Blend</li>
+              <li>Medium Range Forecast</li>
+              <li>Long Range Forecast</li>
+            </ul>
+          </p>
+          <p>For more information:</p>
+          <ul>
+            <li><a href="https://api.water.noaa.gov/nwps/v1/docs/">API Docs</a></li>
+            <li><a href="https://www.weather.gov/media/owp/oh/docs/NWPS_API_Flyer_V6.pdf">API Flyer</a></li>
+          </ul>
+          <h5>Regions</h5>
+          <p>Regions are created by the user using a geometry (*.geojson, *.geopkg, *.shp ) or list of reaches (*.csv) in order to create a <a href="https://www.hydroshare.org/home/"> HydroShare </a> resource with two files: 
+            <ul>
+              <li><em>reaches_nhd_data.csv</em></li>
+              <li><em>nwm_commids.json</em></li>
+            </ul>          
+          </p>
+          <p>A region can be imported from a <a href="https://www.hydroshare.org/home/"> HydroShare </a> resource containing this two files</p>
+          <p>Once the region is created, the long range forecast mean can be accessed for every 50 reaches using the <a href="https://nwm-api.ciroh.org/docs"> National Water Model API </a> </p>
+          <h5>More Information</h5>
+          <p><a href="https://github.com/Aquaveo/OWP"> Source Code </a></p>
         </NavMenu>
         <Routes>
           {routes}
--- tethysapp/owp/app.py
+++ tethysapp/owp/app.py
@@ -12,7 +12,7 @@ class Owp(TethysAppBase):
     description = ""
     package = "owp"  # WARNING: Do not change this value
     index = "home"
-    icon = f"{package}/images/icon.png"
+    icon = f"{package}/images/owp_logo.png"
     catch_all = "home"  # Catch all url mapped to home controller, required for react browser routing
     root_url = "owp"
     color = ""  # Don't set color here, set it in reactapp/custom-bootstrap.scss
